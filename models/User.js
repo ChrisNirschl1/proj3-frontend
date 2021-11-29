5bae384d552061a1a706c7ff0930afca490b1bbf
Chris <chris.feight@gmail.com>
2021-11-29 13:21:24 -0800

    user model & connection built

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPassword) {
+        return bcrypt.compareSync(loginPassword, this.password);
+    }
+}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    user_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user_email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true,
+        },
+    },
+    user_password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            length: [12],
+        },
+    }
+});
+
+module.exports = User;
