e59312732cbc0778b7fbbd05869d3e5222c1ac8b
Christian Nirschl <christiannirschl6@gmail.com>
2021-12-03 11:15:57 -0800

    lots of updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import React from 'react';
+
+import React, { useState, useEffect } from 'react';
+//will need useEffect for token
+import LoginForm from "./components/LoginForm";
+// const Axios = require ("axios"); 
+import axios from "axios";
 import {
   BrowserRouter as Router,
-  Routes,
+  Switch,
   Route,
   Link,
 } from 'react-router-dom';
@@ -9,37 +14,141 @@ import {
 import Home from "./pages/home/Home";
 //import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
- import Login from "./pages/login/Login";
- import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Profile from "./pages/profile/Profile";
 // import Register from "./pages/register/Register";
 
 
 function App() {
+  const [userState, setUserState] = useState({
+    email: "",
+    id: 0
+    //was password instead of id
+  })
+  //token
+  const [token, setToken] = useState("")
+
+  const [loginFormState, setLoginFormState] = useState({
+    email: "",
+    password: "",
+  })
+  //Token validation function
+  useEffect(() => {
+    const myToken = localStorage.getItem("token");
+    if (myToken) {
+      axios.get("http://localhost:8080/api/auth/profile", {
+        headers: {
+          "Authorization": `Bearer ${myToken}`
+        }
+      }).then(res => {
+        console.log("----------", res)
+        setToken(myToken)
+        setUserState({
+          email: res.data.email,
+          id: res.data.id
+        })
+      }).catch(err => {
+        console.log(err)
+        // localStorage.removeItem("token")
+      })
+    }
+  }, [])
+
+  const handleLoginChange = event => {
+    if (event.target.name === "email") {
+      setLoginFormState({
+        ...loginFormState,
+        email: event.target.value
+      })
+    } else {
+      setLoginFormState({
+        ...loginFormState,
+        password: event.target.value
+      })
+    }
+  }
+
+
+  const handleLoginSubmit = e => {
+    e.preventDefault();
+    axios.post("http://localhost:8080/api/auth/login", loginFormState).then(res => {
+      console.log(res.data)
+      setUserState({
+        email: res.data.user.email,
+        id: res.data.user._id
+        //could need to be id
+      })
+      //token
+      setToken(res.data.token)
+      localStorage.setItem("token", res.data.token)
+    }).catch(err => {
+      console.log(err)
+    })
+
+  }
+
+  const logMeOut = () => {
+    setUserState({ email: '', id: 0 })
+    //token
+    setToken("");
+    localStorage.removeItem("token")
+
+  }
+
   return (
+
     <Router>
+
       <div>
-        <nav>
+        {!userState.email ? (
+          <div><nav>
           <ul>
+            <button onClick={logMeOut}>Log out</button>
             <li>
               <Link to="/">Home</Link>
             </li>
             <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
               <Link to="/register">Register</Link>
+            </li> <li>
+              <Link to="/login">Login</Link>
             </li>
             <li>
               <Link to="/profile">Profile</Link>
             </li>
           </ul>
         </nav>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-          <Route exact path="/profile" element={<Profile />} />
-        </Routes>
+            <LoginForm submit={handleLoginSubmit} change={handleLoginChange} loginState={loginFormState} />
+          </div>
+        ) : (
+          <div>
+            <nav>
+              <ul>
+                <button onClick={logMeOut}>Log out</button>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li> <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/profile">Profile</Link>
+                </li>
+              </ul>
+            </nav>
+            <Switch>
+              <Route exact path="/">
+                <Home token={token} user={userState} />
+              </Route>
+              <Route exact path="/login"></Route>
+              <Route exact path="/register"></Route>
+              <Route exact path="/profile">
+                <Profile token={token} user={userState} />
+              </Route>
+            </Switch>
+          </div>)}
+
       </div>
     </Router>
   );
