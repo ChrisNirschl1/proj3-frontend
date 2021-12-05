37008978d9d4423febd55f926acdaff2ebc3d2e3
Christian Nirschl <christiannirschl6@gmail.com>
2021-12-04 17:47:56 -0800

    create users

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,30 @@
 import axios from "axios";
+//local
+const URL_PREFIX = "http://localhost:3001"
+//delploy
+// const URL_PREFIX = "https://reactauthdemo-back.herokuapp.com"
+
 
 const API = {
-getPRofile: ()=>{
-    return
+    getProfile: (tkn)=>{
+        return axios.get(`${URL_PREFIX}/profile`,{headers:{
+        "Authorization": `Bearer ${tkn}`
+      }})
+    },
+    login:(usrData)=>{
+        return  axios.post(`${URL_PREFIX}/login`,usrData)
+    },
+    signup:(usrData)=>{
+        return  axios.post(`${URL_PREFIX}/signup`,usrData)
+    },
+    // getUserData:(id)=>{
+    //     return axios.get(`${URL_PREFIX}/api/users/${id}/pets`)
+    // },
+ 
 }
-}
+
+
+
+
+
+export default API;
